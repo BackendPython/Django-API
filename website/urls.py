7008b1d870a6e7ae72c51405ddda6c3f95d7c70b
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-07 16:31:08 +0500

    main

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', home),
-    path('adidas/', filter_adidas, name='filter-adidas'),
-    path('nike/', filter_nike, name='filter-nike'),
-    path('add/', postPaste, name='filter-nike'),
     path('product-<int:pk>', singleapi, name='single-api'),
+    path('adidas/', filter_adidas, name='filter-adidas'),
+    path('<int:pk>-edit', postEdit, name='post-edit'),
+    path('nike/', filter_nike, name='filter-nike'),
+    path('add/', postPaste, name='product-add'),
 ]
